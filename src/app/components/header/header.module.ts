c69a91e08cc3e3396ac2112e4bbc65f30e6140f6
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-11 09:21:21 +0430

    [ADD] header structure

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -1,10 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+
 import {HeaderComponent} from './header.component';
+import {SearchBoxComponent} from './components/search-box/search-box.component';
+import {DividerModule} from '../divider/divider.module';
+import {ProfileButtonComponent} from './components/profile-button/profile-button.component';
+import {ButtonModule} from '../button/button.module';
 
 @NgModule({
-    declarations: [HeaderComponent],
+    declarations: [HeaderComponent, SearchBoxComponent, ProfileButtonComponent],
     exports: [HeaderComponent],
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule, DividerModule, ButtonModule],
 })
 export class HeaderModule {}
