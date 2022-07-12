affc303ed7124969d37f8d590267b7e215c84c1e
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-12 13:26:11 +0430

    [ADD] home - categories

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,9 +4,11 @@ import {HomeComponent} from './home.component';
 import {SlideshowComponent} from './components/slideshow/slideshow.component';
 import {HeaderModule} from '../../components/header/header.module';
 import {FooterModule} from '../../components/footer/footer.module';
+import {CategoriesComponent} from './components/categories/categories.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
-    declarations: [HomeComponent, SlideshowComponent],
-    imports: [CommonModule, HeaderModule, FooterModule],
+    declarations: [HomeComponent, SlideshowComponent, CategoriesComponent],
+    imports: [CommonModule, HeaderModule, FooterModule, RouterModule],
 })
 export class HomeModule {}
