f937ee32ee66a8c3078f00a4a764704b4181ed5e
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-19 08:11:16 +0430

    [REMOVE] redundant code

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -6,7 +6,6 @@ import {HeaderComponent} from './header.component';
 import {SearchBoxComponent} from './components/search-box/search-box.component';
 import {DividerModule} from '../divider/divider.module';
 import {ProfileButtonComponent} from './components/profile-button/profile-button.component';
-import {ButtonModule} from '../button/button.module';
 import {FormsModule} from '@angular/forms';
 import {NgxPopperjsModule} from 'ngx-popperjs';
 import {MegaNavComponent} from './components/mega-nav/mega-nav.component';
@@ -14,6 +13,6 @@ import {MegaNavComponent} from './components/mega-nav/mega-nav.component';
 @NgModule({
     declarations: [HeaderComponent, SearchBoxComponent, ProfileButtonComponent, MegaNavComponent],
     exports: [HeaderComponent],
-    imports: [CommonModule, RouterModule, DividerModule, ButtonModule, FormsModule, NgxPopperjsModule],
+    imports: [CommonModule, RouterModule, DividerModule, FormsModule, NgxPopperjsModule],
 })
 export class HeaderModule {}
