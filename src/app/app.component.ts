1d4750c348682c7df50a11640ae0e99cb917c969
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-23 20:18:31 +0430

    [CI-CD][ADD] test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import {Component} from '@angular/core';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-    title = 'Summer1401-Front-End-Tutorial-Project-Sample';
-}
+export class AppComponent {}
