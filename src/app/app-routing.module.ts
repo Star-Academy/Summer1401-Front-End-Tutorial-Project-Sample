f36b459b699770686695b567a13745d77eed43ed
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-25 07:32:32 +0430

    [ADD] profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {NotFoundComponent} from './pages/not-found/not-found.component';
+import {ProfileComponent} from './pages/profile/profile.component';
 
 const routes: Routes = [
     {path: '', pathMatch: 'full', component: HomeComponent},
+    {path: 'profile', component: ProfileComponent},
     {path: '**', component: NotFoundComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeModule} from './pages/home/home.module';
 import {NotFoundModule} from './pages/not-found/not-found.module';
+import {ProfileModule} from './pages/profile/profile.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, HomeModule, NotFoundModule],
+    imports: [BrowserModule, AppRoutingModule, HomeModule, ProfileModule, NotFoundModule],
     providers: [],
     bootstrap: [AppComponent],
 })
