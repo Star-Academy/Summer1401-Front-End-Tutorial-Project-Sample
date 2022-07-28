2bdfb023bb25bd1b8c328c389987983ab0bf6d76
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-28 10:45:07 +0430

    [ADD] auth guard

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,12 +3,12 @@ import {API_USER_AUTH, API_USER_LOGIN} from '../utils/api.utils';
 import {ApiService} from './api.service';
 import {TokenObject} from '../models/token-object.model';
 import {User} from '../models/user.model';
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Router} from '@angular/router';
 
 @Injectable({
     providedIn: 'root',
 })
-export class AuthService implements CanActivate {
+export class AuthService {
     public constructor(private router: Router, private apiService: ApiService) {}
 
     public async isLoggedIn(): Promise<boolean> {
@@ -21,16 +21,6 @@ export class AuthService implements CanActivate {
         if (!response) return false;
 
         localStorage.setItem('token', response.token);
-        return true;
-    }
-
-    public async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-        const isLoggedIn = await this.isLoggedIn();
-        if (!isLoggedIn) {
-            await this.router.navigateByUrl('/auth');
-            return false;
-        }
-
         return true;
     }
 }
