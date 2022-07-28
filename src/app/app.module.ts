3c25c9c832dbe475f5b75ccc7ffa3dbfa4cffd3e
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-28 14:07:46 +0430

    [ADD] header to all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,22 @@ import {HomeModule} from './pages/home/home.module';
 import {NotFoundModule} from './pages/not-found/not-found.module';
 import {ProfileModule} from './pages/profile/profile.module';
 import {SnackbarModule} from './components/snackbar/snackbar.module';
+import {HeaderModule} from './components/header/header.module';
+import {FooterModule} from './components/footer/footer.module';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, AuthModule, HomeModule, NotFoundModule, ProfileModule, SnackbarModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AuthModule,
+        HomeModule,
+        NotFoundModule,
+        ProfileModule,
+        SnackbarModule,
+        HeaderModule,
+        FooterModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,13 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {SlideshowComponent} from './components/slideshow/slideshow.component';
-import {HeaderModule} from '../../components/header/header.module';
-import {FooterModule} from '../../components/footer/footer.module';
 import {CategoriesComponent} from './components/categories/categories.component';
 import {RouterModule} from '@angular/router';
 
 @NgModule({
     declarations: [HomeComponent, SlideshowComponent, CategoriesComponent],
-    imports: [CommonModule, HeaderModule, FooterModule, RouterModule],
+    imports: [CommonModule, RouterModule],
 })
 export class HomeModule {}
