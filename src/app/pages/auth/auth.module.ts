3f758fa38c12676f3c470cd34c44262c876cc930
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-29 08:13:20 +0430

    [ADD] password input

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -6,9 +6,10 @@ import {RouterModule} from '@angular/router';
 import {LoginFormComponent} from './components/login-form/login-form.component';
 import {RegisterFormComponent} from './components/register-form/register-form.component';
 import {DividerModule} from '../../components/divider/divider.module';
+import {PasswordInputModule} from '../../components/password-input/password-input.module';
 
 @NgModule({
     declarations: [AuthComponent, LoginFormComponent, RegisterFormComponent],
-    imports: [CommonModule, FormsModule, RouterModule, DividerModule],
+    imports: [CommonModule, FormsModule, RouterModule, DividerModule, PasswordInputModule],
 })
 export class AuthModule {}
