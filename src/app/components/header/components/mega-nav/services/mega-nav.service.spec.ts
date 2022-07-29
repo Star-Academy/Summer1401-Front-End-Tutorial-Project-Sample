d36652135ee84ad82b20cf52a70d85d88381151c
BijanEisapour <bijanprogrammer@gmail.com>
2022-07-29 10:33:19 +0430

    [REMOVE] tests

--- src/app/components/header/components/mega-nav/services/mega-nav.service.spec.ts
+++ src/app/components/header/components/mega-nav/services/mega-nav.service.spec.ts
@@ -1,16 +1,1 @@
-import {TestBed} from '@angular/core/testing';
-
-import {MegaNavService} from './mega-nav.service';
-
-describe('MegaNavService', () => {
-    let service: MegaNavService;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(MegaNavService);
-    });
-
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
-});
+describe('MegaNavService', () => {});
